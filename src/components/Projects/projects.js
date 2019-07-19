dc394d866d82fa9fcf76b4cb0d8b0837f1225993
Justin Ashcraft <justinashcraft@yahoo.com>
2019-07-19 15:43:58 -0700

    Removing useless wrapper container

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -49,84 +49,66 @@ function Projects() {
     const classes = useStyles();
     return <section>
         <CssBaseline />
-        <Container maxWidth="md">
-            <Grid container className={classes.root} spacing={3}>
-                <Grid item >
-                    {/* <Grid container spacing={4}> */}
+        
+        <Container maxWidth="md" className={classes.cardGrid}>
+            <h1>Projects</h1>
+            <Card className={classes.card}>
 
-                    {/* <Grid item> */}
-                    {/* <Paper> */}
-
-                    {/* <img src={img_cg_rpenroll} alt="Capital Group Retirement Planning Enrollment" />
-                                <img src="http://placehold.it/200x200?text=1" alt="" /> */}
-
-                    <Container maxWidth="md" className={classes.cardGrid}>
-                        <h1>Projects</h1>
-                        <Card className={classes.card}>
-
-                            <CardMedia
-                                className={classes.cardMedia}
-                                image={img_cg_rpenroll}
-                                title="Image title"
-                            />
-                            <CardContent className={classes.cardContent}>
-                                <Typography gutterBottom variant="h5" component="h2">
-                                    Retirement Plan Enroll​ment Micro-sites
+                <CardMedia
+                    className={classes.cardMedia}
+                    image={img_cg_rpenroll}
+                    title="Image title"
+                />
+                <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Retirement Plan Enroll​ment Micro-sites
                                             </Typography>
-                                <Typography>
-                                    This project helped aliviate millions of dollars the business spent creating custom printed retirement participant booklets.
-                                    The Angular single-page application queries a REST API and returns secific plan details. Using this data the application then
-                                    calls a back-end Adobe Experience Manager content repository and dynamically generates content specific to the customer.
+                    <Typography>
+                        This project helped aliviate millions of dollars the business spent creating custom printed retirement participant booklets.
+                        The Angular single-page application queries a REST API and returns secific plan details. Using this data the application then
+                        calls a back-end Adobe Experience Manager content repository and dynamically generates content specific to the customer.
                                             </Typography>
-                            </CardContent>
-                        </Card>
-                    </Container>
+                </CardContent>
+            </Card>
+        </Container>
 
-                    <Container maxWidth="md" className={classes.cardGrid}>
-                        <Card className={classes.card}>
+        <Container maxWidth="md" className={classes.cardGrid}>
+            <Card className={classes.card}>
 
-                            <CardMedia
-                                className={classes.cardMedia}
-                                image={img_cg_autoagreements}
-                                title="Image title"
-                            />
-                            <CardContent className={classes.cardContent}>
-                                <Typography gutterBottom variant="h5" component="h2">
-                                    Building Better Portfolios
+                <CardMedia
+                    className={classes.cardMedia}
+                    image={img_cg_autoagreements}
+                    title="Image title"
+                />
+                <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Building Better Portfolios
                                             </Typography>
-                                <Typography>
-                                    Angular 4+ single page application that compared index funds with American Funds
-                                allowing Advisors to compare past performance. Specifically build for iPad and offline capabilities.
+                    <Typography>
+                        Angular 4+ single page application that compared index funds with American Funds
+                    allowing Advisors to compare past performance. Specifically build for iPad and offline capabilities.
                                             </Typography>
-                            </CardContent>
-                        </Card>
-                    </Container>
+                </CardContent>
+            </Card>
+        </Container>
 
-                    <Container maxWidth="md" className={classes.cardGrid}>
-                        <Card className={classes.card}>
+        <Container maxWidth="md" className={classes.cardGrid}>
+            <Card className={classes.card}>
 
-                            <CardMedia
-                                className={classes.cardMedia}
-                                image="http://placehold.it/1024x768?text=Coming Soon"
-                                title="Image title"
-                            />
-                            <CardContent className={classes.cardContent}>
-                                <Typography gutterBottom variant="h5" component="h2">
-                                    Transaction Auto Agreements
+                <CardMedia
+                    className={classes.cardMedia}
+                    image="http://placehold.it/1024x768?text=Coming Soon"
+                    title="Image title"
+                />
+                <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Transaction Auto Agreements
                                             </Typography>
-                                <Typography>
-                                    Angular application to allowed shareholders to create automated purchase agreements at a scheduled time periods.
+                    <Typography>
+                        Angular application to allowed shareholders to create automated purchase agreements at a scheduled time periods.
                                             </Typography>
-                            </CardContent>
-                        </Card>
-                    </Container>
-
-                    {/* </Paper> */}
-
-                </Grid>
-                {/* </Grid> */}
-                {/* </Grid> */}
-            </Grid>
+                </CardContent>
+            </Card>
         </Container>
 
     </section>
